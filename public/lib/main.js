37f7f49fc9aad46c4396a70b978804caf5124b71
psychobunny <psycho.bunny@hotmail.com>
2014-04-01 13:16:09 -0400

    updated to 0.4x compatbility

--- public/lib/main.js
+++ public/lib/main.js
@@ -5,10 +5,10 @@
 		$(window).on('action:ajaxify.end', function(ev, data) {
 			if (data.url.match(/^topic/)) {
 				$('.thread-tools .mark-featured').on('click', function(ev) {
-					templates.preload_template('modals/sort-featured-topics', function() {
-						socket.emit('topics.getFeaturedTopics', {tid: templates.get('topic_id')}, function(err, topics) {
+					ajaxify.loadTemplate('modals/sort-featured-topics', function(featuredTpl) {
+						socket.emit('topics.getFeaturedTopics', {tid: ajaxify.variables.get('topic_id')}, function(err, topics) {
 							if (!err) {
-								bootbox.confirm(templates['modals/sort-featured-topics'].parse({topics:topics}), function(confirm) {
+								bootbox.confirm(templates.parse(featuredTpl, {topics:topics}), function(confirm) {
 									var tids = [];
 									$('.featured-topic').each(function(i) {
 										tids.push(this.getAttribute('data-tid'));
