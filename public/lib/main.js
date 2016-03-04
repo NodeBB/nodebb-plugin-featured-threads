6fb9ea07fc21dbabe8ec009fdbac3edd05ed8564
yariplus <tafike@gmail.com>
2016-03-04 04:02:04 -0500

    Cleaning up main.js
    No need to triple wrap, event will never be called before document ready.
    Added a bs callback.
    Removed event prevention. Doesn't do anything but make the menu not disappear.

--- public/lib/main.js
+++ public/lib/main.js
@@ -1,42 +1,29 @@
-(function() {
+$(window).on('action:ajaxify.end', function(ev, data) {
 	"use strict";
 
-	jQuery('document').ready(function() {
-		$(window).on('action:ajaxify.end', function(ev, data) {
-			if (data.url.match(/^topic/)) {
-				$('.topic').on('click', '.thread-tools .mark-featured', function(ev) {
-					ajaxify.loadTemplate('modals/sort-featured-topics', function(featuredTpl) {
-						socket.emit('topics.getFeaturedTopics', {tid: ajaxify.data.tid}, function(err, topics) {
-							if (!err) {
-								bootbox.confirm(templates.parse(featuredTpl, {topics:topics}), function(confirm) {
-									var tids = [];
-									$('.featured-topic').each(function(i) {
-										tids.push(this.getAttribute('data-tid'));
-									});
+	if (data.url.match(/^topic/)) {
+		$('.topic').on('click', '.thread-tools .mark-featured', function(ev) {
+			ajaxify.loadTemplate('modals/sort-featured-topics', function(featuredTpl) {
+				socket.emit('topics.getFeaturedTopics', {tid: ajaxify.data.tid}, function(err, topics) {
+					if (err) return console.log(err);
 
-									socket.emit('topics.setFeaturedTopics', {tids: tids});
-								});
+					bootbox.confirm(templates.parse(featuredTpl, {topics:topics}), function(confirm) {
+						var tids = [];
+						$('.featured-topic').each(function(i) {
+							tids.push(this.getAttribute('data-tid'));
+						});
 
-								setTimeout(function() {
-									// wtb callback for bootbox...
-									$('span.timeago').timeago();
-									$('#sort-featured').sortable().disableSelection();
+						socket.emit('topics.setFeaturedTopics', {tids: tids});
+					}).on("shown.bs.modal", function() {
+						$('span.timeago').timeago();
+						$('#sort-featured').sortable().disableSelection();
 
-									$('.delete-featured').on('click', function() {
-										$(this).parents('.panel').remove();
-									});
-								}, 500);
-							}else{
-								console.log(err);
-							}
+						$('.delete-featured').on('click', function() {
+							$(this).parents('.panel').remove();
 						});
 					});
-
-					ev.preventDefault();
-					return false;
 				});
-
-			}
+			});
 		});
-	});
-}());
+	}
+});
