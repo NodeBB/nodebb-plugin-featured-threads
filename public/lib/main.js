7d0c69a71e8fff8ab7da74726ce9fe4355e6107a
psychobunny <rodrigues.andrew@gmail.com>
2017-09-28 17:55:22 -0400

    another fix for bch

--- public/lib/main.js
+++ public/lib/main.js
@@ -3,25 +3,25 @@ $(window).on('action:ajaxify.end', function(ev, data) {
 
 	if (data.url.match(/^topic/)) {
 		$('.topic').on('click', '.thread-tools .mark-featured', function(ev) {
-			ajaxify.loadTemplate('modals/sort-featured-topics', function(featuredTpl) {
-				socket.emit('topics.getFeaturedTopics', {tid: ajaxify.data.tid}, function(err, topics) {
-					if (err) return console.log(err);
+		socket.emit('topics.getFeaturedTopics', {tid: ajaxify.data.tid}, function(err, topics) {
+				if (err) {
+					return console.log(err);
+				}
 
-					templates.parse(featuredTpl, {topics:topics}, function(tpl) {
-						bootbox.confirm(tpl, function(confirm) {
-							var tids = [];
-							$('.featured-topic').each(function(i) {
-								tids.push(this.getAttribute('data-tid'));
-							});
-	
-							socket.emit('topics.setFeaturedTopics', {tids: tids});
-						}).on("shown.bs.modal", function() {
-							$('span.timeago').timeago();
-							$('#sort-featured').sortable().disableSelection();
-	
-							$('.delete-featured').on('click', function() {
-								$(this).parents('.panel').remove();
-							});
+				templates.parse('modals/sort-featured-topics', {topics:topics}, function(tpl) {
+					bootbox.confirm(tpl, function(confirm) {
+						var tids = [];
+						$('.featured-topic').each(function(i) {
+							tids.push(this.getAttribute('data-tid'));
+						});
+
+						socket.emit('topics.setFeaturedTopics', {tids: tids});
+					}).on("shown.bs.modal", function() {
+						$('span.timeago').timeago();
+						$('#sort-featured').sortable().disableSelection();
+
+						$('.delete-featured').on('click', function() {
+							$(this).parents('.panel').remove();
 						});
 					});
 				});
